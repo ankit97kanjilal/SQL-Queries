c0d58bb1ebb363f76acefe52229fcdddb8f2e308
ankit97kanjilal <ankitkanjilal.97@gmail.com>
2021-03-08 11:39:38 +0530

    Updated upto 08-MAR-2021 11.40AM

--- Queries/SQLQuery1_For_Regular_new_topics.sql
+++ Queries/SQLQuery1_For_Regular_new_topics.sql
@@ -234,3 +234,202 @@ select * from emp_v1
 --DML thru views
 insert into emp_v1 values(108,'Ramnath')
 select * from employee
+
+use ITCDB
+--=================Looping in TSQL========================
+declare @n int, @p int, @i int
+select @n=5, @i=1
+while @i <= 10
+begin
+	select @p = @n * @i
+	--select @p
+	--print @p	--display as character... Print is only used for character
+	--print @n + 'X' + @i + '=' + @p	--Can't be converted to varchar
+	print convert(varchar(5),@n) + 'X' + convert(varchar(5),@i) + '=' + convert(varchar(5),@p)
+	select @i = @i + 1
+end
+
+--Break:-is used to conditionally come out of the loop
+--Continue:-is used to skip the remaining task by incrementing the value of counter
+
+/*
+Two types of table-values functions:
+1) Inline table-valued functions:-only one return statement with TABLE result is allowed
+2) Multiline-Statement Table-Values functions(MSTVFs):-
+
+
+a)Declare the TABLE structure as variable as variable of TABLE type
+b)The variable is populated with multiple DML statements
+c) The final TABLE type variable is returned
+
+syntax=>
+--old staffs
+--new staffs
+
+create a function and return all those staffs based on category like old or new staff */
+
+create table old_staffs
+(
+	staffid varchar(10),
+	staffname varchar(20),
+	doj date,
+	category varchar(20)
+)
+
+create table new_staffs
+(
+	staffid varchar(10),
+	staffname varchar(20),
+	doj date,
+	category varchar(20)
+)
+
+insert into old_staffs values('s1','Ravi','26-MAR-2018','Clerk')
+insert into old_staffs values('s2','Ramcharan','25-MAR-2018','Analyst')
+insert into old_staffs values('s3','Ram','22-MAR-2018','Manager')
+insert into old_staffs values('s4','Raghu','20-MAR-2018','Clerk')
+
+insert into new_staffs values('s5','Raaaja','16-MAR-2018','Clerk')
+insert into new_staffs values('s6','Rashmi','15-MAR-2018','Analyst')
+insert into new_staffs values('s7','Rashford','12-MAR-2018','Manager')
+insert into new_staffs values('s8','R.Bir','10-MAR-2018','Clerk')
+
+
+alter function fn_staffs_mstvf(@category varchar(20))
+returns @output TABLE
+(
+	staffid varchar(10),
+	staffname varchar(20),
+	doj date,
+	category varchar(20)
+)
+as
+begin
+insert into @output select staffid,staffname,doj,category from old_staffs where category=@category
+insert into @output select staffid,staffname,doj,category from new_staffs where category=@category
+UPDATE @output SET staffname=UPPER(staffname)
+return
+end
+
+select * from fn_staffs_mstvf('Clerk')
+
+--======By using begin and end we can use the thing like a block========
+
+--=========CTE(Common Table Expression)==============
+------------------Intermediate Result(By using CTE--Then some more query and get result)-------------
+--selec statement----gives result---some more query on this result
+--Example
+select deptid,COUNT(deptid),SUM(salary) from employee
+group by deptid having sum(salary)>1000
+
+--how many departments have their salary by deptid > 1000
+with c1(did)--should match column no here
+as(select deptid --here how many columns we use
+from employee
+group by deptid 
+having sum(salary)>1000
+)
+select COUNT(did) as [No of Dept] from c1--using CTE we are simplyfying the query
+
+/*with c0(did,totalsal)
+as(
+	with c1(did,totalsal)
+	as(
+		select deptid,sum(salary)										--Look at this later
+		from employee
+		group by deptid
+	)
+	select did,totalsal from c1 where totalsal>1000
+)
+select * from c0*/
+
+--==================================TRIGGERS==============================================
+--Triggers: These are TSQL block of statements which gets triggered or fired whenever the action on
+--the darabase or table is perfomred on which the trigger is defined.
+--Usage:-
+--1)To control some unwanted data manipulations by revarting it back, hence used as data-security.
+--2)To perform some automatic calculation on some operations without doing manually.
+/*	types of triggers:
+1. DDL Triggers:-defined for DDL statement, hence also called database triggers
+2. DML triggers:-defined on tables for INSERT, DELETE, UPDATE operations
+*/
+alter trigger no_delete_table_trig on database
+for DROP_TABLE
+as
+	--statements to be fired when triggers is called
+	rollback tran --when we try to 
+	print 'Trigger fired'
+	print 'U cannot drop table in this database, contact ur admin'
+
+drop table employee
+
+create trigger no_create_table_trig on database
+for CREATE_TABLE
+as
+	--statements to be fired when triggers is called
+	rollback tran --when we try to 
+	print 'Trigger fired'
+	print 'U cannot create table in this database, contact ur admin'
+
+drop table employee--The transaction ended in the trigger. The batch has been aborted.
+create table xyz
+(
+	id int,
+	roll int		--The transaction ended in the trigger. The batch has been aborted.
+)
+
+--dropping the trigger
+drop trigger no_create_table_trig on database
+drop trigger no_delete_table_trig on database
+
+create table xyz
+(
+	id int,
+	roll int		--Now can be created
+)
+drop table xyz --Now can be dropped
+
+--==2.DML triggers:
+alter trigger trig1_emp
+on employee
+for delete,insert
+as
+	rollback
+	print 'U cannot delete/insert records from this table'
+
+select * from employee where ecode=101
+delete from employee where ecode=101
+select * from employee
+insert into employee values(115,'Ravicharan',1050,206)
+drop trigger trig1_emp
+
+--Qs..
+
+create table log_table
+(
+	ecode int,
+	ename varchar(20),
+	salary int,
+	deptid int,
+	dot datetime
+)
+
+create trigger trig_del_emp
+on employee
+for delete
+as
+--trigger context block
+	declare @ec int, @en varchar(20), @sal int, @did int
+	select @ec=ecode,@en=ename,@sal=salary,@did=deptid from deleted
+	--insert the deleted records into log table
+	insert into log_table values(@ec,@en,@sal,@did,getdate())
+	print 'record deleted and logged into log table'
+--context block ends here	
+
+/*magical or virtual table in the trigger context block:-
+1)deleted:-
+2)inserted:-*/
+select * from employee
+delete from employee where ecode=106
+select * from log_table
+delete from employee where deptid=202--more than one record deleted but one record is inserted
